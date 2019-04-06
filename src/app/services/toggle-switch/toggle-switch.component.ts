2852b8a7163dfdb7ad4c83bd66048b01bda75bc0
Wajeeh Misbah Khan <wajeehmisbahkhan@yahoo.com>
2019-04-06 12:36:36 +0500

    Adding directive for switch

--- src/app/services/toggle-switch/toggle-switch.component.ts
+++ src/app/services/toggle-switch/toggle-switch.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-toggle-switch',
+  selector: 'toggle-switch',
   templateUrl: './toggle-switch.component.html',
   styleUrls: ['./toggle-switch.component.scss'],
 })
 export class ToggleSwitchComponent implements OnInit {
 
+  private onText: string;
+  private offText: string;
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    
+  }
 
 }
