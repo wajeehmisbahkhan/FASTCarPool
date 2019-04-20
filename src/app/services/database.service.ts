374cb388f2ad9445383a86189c9d3cc7307924b5
Wajeeh Misbah Khan <wajeehmisbahkhan@yahoo.com>
2019-04-20 20:38:10 +0500

    On the right track but ngModel and reactive forms are not supported together

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { firestore } from 'firebase/app';
+import { User } from './helper-classes';
 
 @Injectable({
   providedIn: 'root'
@@ -14,38 +14,7 @@ export class DatabaseService {
 
   createNewUser(email: string) {
     // Add to users folder - reference by email users/email.get(property)
-    this.db.collection('users').doc(email).set({
-        chats: [],
-        driver: false,
-        status: 'Hey there! I\'m using FAST CarPool.',
-        home: {
-            lat: 24.8607,
-            lng: 67.0011
-        },
-        location: { // Last seen
-            lat: 24.8607,
-            lng: 67.0011
-        },
-        schedule: {
-            // 8 slots every day
-            monday: [null, null, null, null, null, null, null, null],
-            tuesday: [null, null, null, null, null, null, null, null],
-            wednesday: [null, null, null, null, null, null, null, null],
-            thursday: [null, null, null, null, null, null, null, null],
-            friday: [null, null, null, null, null, null, null, null]
-        },
-        car: {
-            capacity: 0,
-            riders: 0,
-            description: ''
-        },
-        price: {
-            oneway: 50,
-            daily: 100,
-            weekly: 500,
-            semester: 8000
-        }
-    });
+    this.setDoc(`users/${email}`, (new User).toObject());
   }
 
   getCollection(path: string, options?: firebase.firestore.GetOptions): Observable<firebase.firestore.QuerySnapshot> {
